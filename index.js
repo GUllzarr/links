f5afb630d0f65d41538cc4d0d58047d9d830fca5
GUllzarr <160250097+GUllzarr@users.noreply.github.com>
2024-09-12 02:48:30 -0700

    links done

---
+++ index.js
@@ -0,0 +1,12 @@
+// html first page code fnction
+// function greetTheUser(){
+//     alert("Hi")
+// }
+
+// // page 2 ftn
+// function sayBye(){
+//     alert("Hi")
+// }
+function addFiveAndTen(a,b) {
+    alert(a + b)
+}
